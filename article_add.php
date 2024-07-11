787169028fa9d8ddb2826c96fd45a4cceaddae8b
Kacper <kacper.tw@wp.pl>
2024-07-11 13:43:05 +0200

    Fixing "hack language" bug

--- article_add.php
+++ article_add.php
@@ -5,21 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Simple blog</title>
     <link rel="stylesheet" href=style.css>
-</head><!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple blog</title>
-    <link rel="stylesheet" href=style.css>
 </head>
 <body>
-<form action="save_article.php" method="post">
-    <label for="title">Title:</label>
-    <input type="text" id="title" name="title" required><br>
-    <label for="content">Content:</label>
-    <textarea id="content" name="content" required></textarea><br>
-    <input type="submit" value="Dodaj artykuł">
-</form>
+<?php
+    echo "Hi";
+?>
+    <form action="save_article.php" method="post">
+        <label for="title">Title:</label>
+        <input type="text" id="title" name="title" required><br>
+        <label for="content">Content:</label>
+        <textarea id="content" name="content" required></textarea><br>
+        <input type="submit" value="Dodaj artykuł">
+    </form>
 </body>
 </html>
