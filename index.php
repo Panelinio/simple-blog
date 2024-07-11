49a10ef22f35dc2128f1d32132e476a5a96fe67a
Kacper <kacper.tw@wp.pl>
2024-07-11 09:02:22 +0200

    Inserting PHP code

--- index.php
+++ index.php
@@ -17,25 +17,17 @@
     <header id="blog"><h1>Simple blog</h1></header>
     <nav>Sharing simple thoughts :D </nav>
     <main>
-        <article>
-            <header class="title"><h2>-Ale fajny tytuł-</h2></header>
-            <p class="author">Żelek</p>
-            <div class="content">
-                Mój stary to fanatyk wędkarstwa. Pół mieszkania zajebane wędkami, najgorzej. Średnio raz w miesiącu ktoś wdepnie w leżący na ziemi haczyk czy kotwicę i trzeba wyciągać w szpitalu, bo mają zadziory na końcu. W swoim dwudziestodwuletnim życiu już z 10 razy byłem na takim zabiegu. Tydzień temu poszedłem na jakieś losowe badania, to baba z recepcji jak mnie tylko zobaczyła, to kazała buta ściągać xD, bo myślała, że znowu hak w nodze.
+    <!--<?php
+        require 'config.php';
 
-            </div>
-            <footer class="bottomContent">
-                <section class="tags">
-                    <ul>
-                        <li>super</li>
-                        <li>ciekawe</li>
-                        <li>tagi</li>
-                    </ul>
-                    <button type="button" class="commenting">Add your comment</button>
-                </section>
-            </footer>
-        </article>
-        <aside class="comments">
+        $stmt = $pdo->query("SELECT * FROM articles ORDER BY created_at DESC");
+        while ($row = $stmt->fetch()) {
+            echo '<article class="main"><article class="sub"><header class="title"><h2>-' . htmlspecialchars($row['title']) . '-</h2></header>';
+            echo '<p class="author"></p>';
+            echo '<div class="content">' . nl2br(htmlspecialchars($row['content'])) . "</div>";
+            echo '<footer class="bottomContent"><section class="tags"><ul><li></li></ul><button type="button" class="commenting">Add your comment</button></section></footer>';
+            echo '</article>';
+            echo '<aside class="comments">
             <p class="author">Comments</p>
             <ul>
                 <li><p class="author">Żelek</p>
@@ -48,7 +40,41 @@
                     <p class="comment">Elo żelo</p>
                 </li>    
             </ul>
+        </aside>';
+            echo '</article>';
+        }
+    ?>-->
+    <article class="main">
+        <article class="sub">
+            <header class="title"><h2>-Your title-</h2></header>
+            <p class="author">Your name/nickname</p>
+            <div class="content"><p>Here goes your text. You can tell us about your yesterday's trip to local cinema, or you can describe your favourite book :D</p><p>Feel free to share your thoughts!</p></div>
+            <footer class="bottomContent">
+                <section class="tags">
+                    <ul>
+                        <li>excellent</li>
+                        <li>interesting</li>
+                        <li>tags</li>
+                    </ul>
+                    <button type="button" class="commenting">Add your comment</button>
+                </section>
+            </footer>
+        </article>
+        <aside class="comments">
+            <p class="author">Comments</p>
+            <ul>
+                <li><p class="author">AuthorOfComment</p>
+                    <p class="comment">His comment</p>
+                </li>
+                <li><p class="author">Jelly</p>
+                    <p class="comment">Hi there</p>
+                </li>
+                <li><p class="author">Your_fan48</p>
+                    <p class="comment">I love you!</p>
+                </li>    
+            </ul>
         </aside>
+    </article>
     </main>
     <article id="addComBack">
         <section id="addCom">
@@ -61,16 +87,7 @@
             </form>
         </section>
     </article>
-    <?php
-        require 'config.php';
-
-        $stmt = $pdo->query("SELECT * FROM articles ORDER BY created_at DESC");
-        while ($row = $stmt->fetch()) {
-            echo "<h2>" . htmlspecialchars($row['title']) . "</h2>";
-            echo "<p>" . nl2br(htmlspecialchars($row['content'])) . "</p>";
-            echo "<hr>";
-        }
-    ?>
+    
     <footer id="madeby">-Kacper Twardowski | 2024-</footer>
     <script src="script.js"></script>
 </body>
